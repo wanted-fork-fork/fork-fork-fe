3aacc743745f5c5886408ca7599c77212d40fa9a
조예진 <orobos654@gmail.com>
2024-05-12 13:26:03 +0900

    chore: add configs layer

--- packages/configs/eslint-config/library.js
+++ packages/configs/eslint-config/library.js
@@ -4,21 +4,15 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "eslint-config-turbo",
-  ],
+  extends: ["eslint:recommended", "prettier", "config-eslint-turbo"],
+  plugins: ["only-warn"],
   globals: {
     React: true,
     JSX: true,
   },
   env: {
     node: true,
-    browser: true,
   },
-  plugins: ["only-warn"],
   settings: {
     "import/resolver": {
       typescript: {
@@ -30,6 +24,11 @@ module.exports = {
     // Ignore dotfiles
     ".*.js",
     "node_modules/",
+    "dist/",
   ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+  overrides: [
+    {
+      files: ["*.js?(x)", "*.ts?(x)"],
+    },
+  ],
 };
