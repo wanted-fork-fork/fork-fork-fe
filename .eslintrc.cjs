e1b0e6dcffccae87209044158213b1ac4ffee9e5
조예진 <orobos654@gmail.com>
2024-06-08 22:59:33 +0900

    chore(eslint): add typescript-eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -86,7 +86,7 @@ module.exports = {
         },
       },
       extends: [
-        // "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended",
         "plugin:import/recommended",
         "plugin:import/typescript",
       ],
