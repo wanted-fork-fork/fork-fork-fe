de3c9bf035e609efb583c8750c438248dd8ba9a4
조예진 <orobos654@gmail.com>
2024-06-20 00:17:33 +0900

    fix: do not recreate data url on uploading new file

--- src/shared/functions/useDataUrlListFromFiles.ts
+++ src/shared/functions/useDataUrlListFromFiles.ts
@@ -1,13 +1,22 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 
 export const useDataUrlListFromFiles = (files: File[]) => {
+  const fileToDataUrlMap = useRef<Map<File, string>>(new Map());
+
   const dataUrlList = useMemo(() => {
-    return files.map((file) => URL.createObjectURL(file));
+    return files.map((file) => {
+      if (!fileToDataUrlMap.current.has(file)) {
+        fileToDataUrlMap.current.set(file, URL.createObjectURL(file));
+      }
+      return fileToDataUrlMap.current.get(file);
+    });
   }, [files]);
 
   useEffect(() => {
-    return () => dataUrlList.forEach((url) => URL.revokeObjectURL(url));
-  }, [dataUrlList]);
+    return () => {
+      Object.values(fileToDataUrlMap).forEach((url) => URL.revokeObjectURL(url));
+    };
+  }, []);
 
   return dataUrlList;
 };
