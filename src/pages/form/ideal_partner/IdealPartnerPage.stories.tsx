d7e893dfff066d0a5d725a0e0cadd63fa87d6abc
조예진 <orobos654@gmail.com>
2024-07-04 01:57:09 +0900

    chore(pages/form): show idealPartner store on story

--- src/pages/form/ideal_partner/IdealPartnerPage.stories.tsx
+++ src/pages/form/ideal_partner/IdealPartnerPage.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { IdealPartnerPage } from 'src/pages/form/ideal_partner/IdealPartnerPage';
+import { useProfileFirstName } from 'src/entities/profile/lib/useProfileFirstName';
+import { useIdlePartnerStore } from 'src/entities/ideal_partner/model/idealPartnerStore';
 
 const meta: Meta<typeof IdealPartnerPage> = {
   component: IdealPartnerPage,
@@ -10,5 +12,19 @@ type Story = StoryObj<typeof IdealPartnerPage>;
 
 export const Default: Story = {
   args: {},
-  decorators: (fn) => <div style={{ height: '100vh' }}>{fn()}</div>,
+  decorators: [
+    (fn) => {
+      const store = useIdlePartnerStore();
+      const firstName = useProfileFirstName();
+      return (
+        <div style={{ height: 'calc(100vh - 32px)', display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+          <div>{fn()}</div>
+          <div>
+            <span>{firstName}</span>
+            <textarea style={{ height: '100%', width: '100%' }} readOnly value={JSON.stringify(store, null, '\t')} />
+          </div>
+        </div>
+      );
+    },
+  ],
 };
