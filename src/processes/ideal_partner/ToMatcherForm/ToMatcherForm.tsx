4e94643cf33a8538599e5482a812eda859ad3cf0
조예진 <orobos654@gmail.com>
2024-07-03 19:26:46 +0900

    feat(processes/ideal): add toMatcherForm

---
+++ src/processes/ideal_partner/ToMatcherForm/ToMatcherForm.tsx
@@ -0,0 +1,19 @@
+import { TextArea } from 'src/shared/ui/TextArea/TextArea';
+import { useIdlePartnerStore } from 'src/entities/ideal_partner/model/idealPartnerStore';
+import styles from './ToMatcherForm.module.css';
+
+export const ToMatcherForm = () => {
+  const value = useIdlePartnerStore((state) => state.toMatchMaker);
+  const setValue = useIdlePartnerStore((state) => state.setToMatchMaker);
+
+  return (
+    <section className={styles.Container}>
+      <TextArea
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder={'이상형 참고 사항, 주선자에게 부탁하는 말 등 최대 500자까지 입력할 수 있습니다.'}
+        maxLength={500}
+      />
+    </section>
+  );
+};
