53a1b734b91145021dd4257633d7e311bc60a1de
조예진 <orobos654@gmail.com>
2024-07-04 00:43:41 +0900

    feat(processes/ideal): add HobbyForm

---
+++ src/processes/ideal_partner/HobbyForm/HobbyForm.stories.ts
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { HobbyForm } from 'src/processes/ideal_partner/HobbyForm/HobbyForm';
+import { Hobby } from 'src/entities/profile/types/hobby';
+
+const meta: Meta<typeof HobbyForm> = {
+  component: HobbyForm,
+};
+
+export default meta;
+type Story = StoryObj<typeof HobbyForm>;
+
+const MockHobbyList: Hobby[] = [
+  { name: '🧗 클라이밍' },
+  { name: '🥐 맛집탐방' },
+  { name: '🎬 영화보기' },
+  { name: '👟 운동' },
+  { name: '🧶 뜨개질' },
+  { name: '🧑‍💻 개발공부' },
+  { name: '📖 독서' },
+  { name: '🍷 와인' },
+  { name: '🏊 수영' },
+  { name: '🏌️ 골프' },
+  { name: '🎹 악기연주' },
+];
+
+export const Default: Story = {
+  args: {
+    exampleHobbyList: MockHobbyList,
+  },
+};
---
+++ src/processes/ideal_partner/HobbyForm/HobbyForm.tsx
@@ -0,0 +1,29 @@
+import styles from './HobbyForm.module.css';
+import { useIdlePartnerStore } from 'src/entities/ideal_partner/model/idealPartnerStore';
+import { ChipList } from 'src/shared/ui/ChipList/ChipList';
+import { Hobby } from 'src/entities/profile/types/hobby';
+
+type HobbyFormProps = {
+  exampleHobbyList?: Hobby[];
+};
+
+export const HobbyForm = ({ exampleHobbyList = [] }: HobbyFormProps) => {
+  const hobbies = useIdlePartnerStore((state) => state.hobbies);
+  const setHobbies = useIdlePartnerStore((state) => state.setHobbies);
+
+  return (
+    <>
+      <section className={styles.Container}>
+        <ChipList
+          defaultList={exampleHobbyList}
+          selectedList={hobbies}
+          setSelectedList={setHobbies}
+          customInputTitle={'추가하실 취미를 입력해주세요.'}
+          customInputPlaceholder={'취미 입력'}
+          makeItem={(name) => ({ name })}
+          hasItem={(list, target) => list.some((item) => item.name === target.name)}
+        />
+      </section>
+    </>
+  );
+};
