82363f6d35302b0acb9d406de9ac0c49340735f5
조예진 <orobos654@gmail.com>
2024-07-04 01:06:02 +0900

    refactor(processes/profile): extract LocationTable

--- src/processes/my_profile/LocationForm/LocationForm.tsx
+++ src/processes/my_profile/LocationForm/LocationForm.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styles from './LocationForm.module.css';
-import { Close, Minus, Plus } from 'src/shared/ui/icons';
+import { Close } from 'src/shared/ui/icons';
 import { Location } from 'src/entities/location/types/location';
-import { useMultiSelectToggle } from 'src/shared/functions/useMultiSelectToggle';
 import { Chip } from 'src/shared/ui/Chip/Chip';
 import { useMyProfileStore } from 'src/entities/profile/model/myProfileStore';
+import { LocationSelectTable } from 'src/widgets/LocationSelectTable/LocationSelectTable';
+import { useMultiSelectToggle } from 'src/shared/functions/useMultiSelectToggle';
 
 type LocationFormProps = {
   locations?: Location[];
 };
 
 export const LocationForm = ({ locations = [] }: LocationFormProps) => {
-  const [selectedMainLocation, setSelectedMainLocation] = useState<Location>(locations[0]);
   const { list: selectedTownList, toggle: toggleTown } = useMultiSelectToggle<Location>(
     [],
     (a, b) => a.town[0]?.town === b.town[0]?.town,
@@ -20,7 +20,7 @@ export const LocationForm = ({ locations = [] }: LocationFormProps) => {
   const setLocation = useMyProfileStore((state) => state.setLocation);
   useEffect(() => {
     setLocation(selectedTownList);
-  }, [selectedTownList]);
+  }, [selectedTownList, setLocation]);
 
   return (
     <section>
@@ -31,37 +31,7 @@ export const LocationForm = ({ locations = [] }: LocationFormProps) => {
           </Chip>
         ))}
       </div>
-      <div className={styles.Wrapper}>
-        <ul className={styles.MainCategoryColumn}>
-          {locations.map((location) => (
-            <li
-              key={location.city.city}
-              className={`${styles.Category} ${styles.MainCategory}`}
-              role={'option'}
-              aria-selected={selectedMainLocation === location}
-              onClick={() => setSelectedMainLocation(location)}
-            >
-              {location.city.cityName}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {selectedMainLocation.town.map((town) => (
-            <li
-              key={town.townName}
-              className={`${styles.Category} ${styles.SubCategory}`}
-              onClick={() => toggleTown({ city: selectedMainLocation.city, town: [town] })}
-            >
-              {town.townName}
-              {selectedTownList.some(({ town: selectedTown }) => selectedTown[0].town === town.town) ? (
-                <Minus className={styles.Icon} width={16} height={16} color={'#cdcace'} />
-              ) : (
-                <Plus className={styles.Icon} width={16} height={16} color={'#cdcace'} />
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <LocationSelectTable locations={locations} selectedLocations={selectedTownList} selectLocation={toggleTown} />
     </section>
   );
 };
---
+++ src/widgets/LocationSelectTable/LocationSelectTable.tsx
@@ -0,0 +1,48 @@
+import styles from './LocationSelectTable.module.css';
+import { Minus, Plus } from 'src/shared/ui/icons';
+import { useState } from 'react';
+import { Location } from 'src/entities/location/types/location';
+
+type Props = {
+  locations: Location[];
+  selectedLocations: Location[];
+  selectLocation: (loc: Location) => void;
+};
+
+export const LocationSelectTable = ({ locations, selectedLocations, selectLocation }: Props) => {
+  const [selectedMainLocation, setSelectedMainLocation] = useState<Location>(locations[0]);
+
+  return (
+    <div className={styles.Wrapper}>
+      <ul className={styles.MainCategoryColumn}>
+        {locations.map((location) => (
+          <li
+            key={location.city.city}
+            className={`${styles.Category} ${styles.MainCategory}`}
+            role={'option'}
+            aria-selected={selectedMainLocation === location}
+            onClick={() => setSelectedMainLocation(location)}
+          >
+            {location.city.cityName}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {selectedMainLocation.town.map((town) => (
+          <li
+            key={town.townName}
+            className={`${styles.Category} ${styles.SubCategory}`}
+            onClick={() => selectLocation({ city: selectedMainLocation.city, town: [town] })}
+          >
+            {town.townName}
+            {selectedLocations.some(({ town: selectedTown }) => selectedTown[0].town === town.town) ? (
+              <Minus className={styles.Icon} width={16} height={16} color={'#cdcace'} />
+            ) : (
+              <Plus className={styles.Icon} width={16} height={16} color={'#cdcace'} />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
