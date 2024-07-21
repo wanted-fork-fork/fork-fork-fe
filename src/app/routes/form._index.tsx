9fe37799c6f0ff5dba9da6cce5d1ac8d5748b09b
조예진 <orobos654@gmail.com>
2024-07-21 19:21:54 +0900

    refactor(app/form): extract step case from component

--- src/app/routes/form._index.tsx
+++ src/app/routes/form._index.tsx
@@ -1,5 +1,5 @@
 import { ProfileFormIntroPage } from 'src/pages/form/intro/ProfileFormIntroPage';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { SwitchCase } from 'src/shared/ui/SwitchCase';
 import { MyProfilePage } from 'src/pages/form/my_profile/MyProfilePage';
 import { IdealPartnerIntroPage } from 'src/pages/form/intro/IdealPartnerIntroPage';
@@ -13,29 +13,29 @@ import styles from 'src/app/styles/form.module.css';
 
 const MAX_STEP_COUNT = 7;
 
-export default function ProfileFormPage() {
-  const [step, setStep] = useState(0);
+const createFormPageStep = ({ name, increase }: { name: string; increase: () => void }) => ({
+  0: <ProfileFormIntroPage onClickNextStep={increase} />,
+  1: <MyProfilePage onClickNextStep={increase} />,
+  2: <IdealPartnerIntroPage name={name} onClickNextStep={increase} />,
+  3: <IdealPartnerPage onClickNextStep={increase} />,
+  4: <FormConfirmPage onClickNextStep={increase} />,
+  5: <UploadLoadingPage name={name} onComplete={increase} />,
+  6: <CompletePage />,
+});
 
+export default function ProfileFormPage() {
+  const name = useProfileFirstName();
+
+  const [step, setStep] = useState(0);
   const increase = () => setStep((prev) => (prev + 1 < MAX_STEP_COUNT ? prev + 1 : prev));
 
-  const name = useProfileFirstName();
+  const formPageStep = useMemo(() => createFormPageStep({ name, increase }), [name]);
 
   const showShortcut = [1, 3, 4].includes(step);
 
   return (
     <div className={styles.Wrapper}>
-      <SwitchCase
-        value={step}
-        caseBy={{
-          0: <ProfileFormIntroPage onClickNextStep={increase} />,
-          1: <MyProfilePage onClickNextStep={increase} />,
-          2: <IdealPartnerIntroPage name={name} onClickNextStep={increase} />,
-          3: <IdealPartnerPage onClickNextStep={increase} />,
-          4: <FormConfirmPage onClickNextStep={increase} />,
-          5: <UploadLoadingPage name={name} onComplete={increase} />,
-          6: <CompletePage />,
-        }}
-      />
+      <SwitchCase value={step} caseBy={formPageStep} />
       {showShortcut && <Shortcut right={'20px'} bottom={'100px'} />}
     </div>
   );
