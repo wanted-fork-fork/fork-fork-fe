103939b821c418520695739d46c19420de47395e
조예진 <orobos654@gmail.com>
2024-07-21 18:17:04 +0900

    feat(app/root): add link to form

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,9 +1,14 @@
 import type { MetaFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
-  return [{ title: '포크포크' }, { name: 'description', content: '포크포크' }];
+  return [{ title: '구구' }, { name: 'description', content: '내 사랑을 구해줄래? 구해줄게!' }];
 };
 
 export default function Index() {
-  return <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}></div>;
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <Link to={'/form'}>프로필 입력하러 가기</Link>
+    </div>
+  );
 }
