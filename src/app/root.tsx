46788e4083b3302c5985651ca12bf3eb783927d0
조예진 <orobos654@gmail.com>
2024-07-24 00:23:36 +0900

    feat: set viewport meta

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -11,7 +11,10 @@ export function Layout({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <Meta />
         <Links />
       </head>
