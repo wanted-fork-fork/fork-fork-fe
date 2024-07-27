8afbc258bc29bcd52fa39dd937ce2201cce94f6e
조예진 <orobos654@gmail.com>
2024-07-27 20:40:03 +0900

    feat: add favicons

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -5,6 +5,29 @@ import 'src/shared/styles/global.css';
 import 'src/shared/styles/variables.css';
 import 'src/shared/styles/typography.css';
 import { WideDeviceLayout } from 'src/pages/layout/WideDeviceLayout';
+import { LinksFunction } from '@remix-run/node';
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'icon',
+      href: '/favicon-32x32.png',
+      sizes: '32x32',
+      type: 'image/png',
+    },
+    {
+      rel: 'icon',
+      href: '/favicon-16x16.png',
+      sizes: '16x16',
+      type: 'image/png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/apple-touch-icon.png',
+      sizes: '180x180',
+    },
+  ];
+};
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
