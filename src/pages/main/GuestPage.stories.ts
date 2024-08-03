b5e668880cbd03b8ef19b61e72b7129beb3f17ad
조예진 <orobos654@gmail.com>
2024-08-03 20:45:56 +0900

    feat: add GuestPage

---
+++ src/pages/main/GuestPage.stories.ts
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { GuestPage } from 'src/pages/main/GuestPage';
+
+const meta: Meta<typeof GuestPage> = {
+  component: GuestPage,
+};
+
+export default meta;
+type Story = StoryObj<typeof GuestPage>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ src/pages/main/GuestPage.tsx
@@ -0,0 +1,25 @@
+import styles from './GuestPage.module.css';
+
+export const GuestPage = () => {
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.ImageWrapper}>
+        <h1>내 사랑 구해줄래? 구해줄게!</h1>
+      </div>
+      <div className={styles.ButtonWrapper}>
+        <button className={`${styles.Button} ${styles.Kakao}`}>
+          <span className={styles.Icon}>
+            <img src="/images/kakao.png" alt="카카오 로그인" width={29} height={29} />
+          </span>
+          카카오톡으로 시작하기
+        </button>
+        <button className={`${styles.Button} ${styles.Apple}`} disabled>
+          <span className={styles.Icon}>
+            <img src="/images/apple.png" alt="카카오 로그인" width={19} height={22} />
+          </span>
+          Apple 계정으로 시작하기
+        </button>
+      </div>
+    </div>
+  );
+};
