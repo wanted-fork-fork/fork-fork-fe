c8345841753a226d8da858c58819c86a2ea16084
조예진 <orobos654@gmail.com>
2024-08-03 21:24:29 +0900

    feat: change main page

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,14 +1,10 @@
 import type { MetaFunction } from '@remix-run/node';
-import { Link } from '@remix-run/react';
+import { GuestPage } from 'src/pages/main/GuestPage';
 
 export const meta: MetaFunction = () => {
   return [{ title: '구구' }, { name: 'description', content: '내 사랑을 구해줄래? 구해줄게!' }];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <Link to={'/form'}>프로필 입력하러 가기</Link>
-    </div>
-  );
+  return <GuestPage />;
 }
