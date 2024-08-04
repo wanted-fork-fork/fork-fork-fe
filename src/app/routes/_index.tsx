e6595e4266b6b6515cd6af79993b18963dec9986
조예진 <orobos654@gmail.com>
2024-08-04 22:45:06 +0900

    feat: add floating button for copying profile form link

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunction, MetaFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 import { InfoListPage } from 'src/pages/main/info_list/InfoListPage';
+import { CopyProfileFormLinkButton } from 'src/features/copy_profile_link/CopyProfileFormLinkButton';
 
 export const meta: MetaFunction = () => {
   return [{ title: '구구' }, { name: 'description', content: '내 사랑을 구해줄래? 구해줄게!' }];
@@ -18,5 +19,10 @@ export const loader: LoaderFunction = ({ request }) => {
 };
 
 export default function Index() {
-  return <InfoListPage />;
+  return (
+    <>
+      <InfoListPage />
+      <CopyProfileFormLinkButton />
+    </>
+  );
 }
