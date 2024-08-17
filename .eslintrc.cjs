7abcf7f69c358cacfaefcb05a501816bf9496f05
조예진 <orobos654@gmail.com>
2024-08-17 20:03:36 +0900

    feat: add react-query eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
   // Base config
   extends: [
     "eslint:recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
     "plugin:storybook/recommended",
     "plugin:storybook/recommended",
     "plugin:storybook/recommended",
