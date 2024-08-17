69b0023765f361542b2930bf78cf133f7d73f161
조예진 <orobos654@gmail.com>
2024-08-17 20:22:10 +0900

    feat: add axios interceptor for authorization

--- src/shared/lib/custom_instance.ts
+++ src/shared/lib/custom_instance.ts
@@ -1,10 +1,70 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { refreshToken } from 'src/types';
+
+const instance = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL });
+
+const getAccessToken = () => localStorage?.getItem('accessToken') ?? null;
+
+instance.interceptors.request.use(
+  (config) => {
+    if (!config.headers) return config;
+
+    let token = null;
+
+    if (!config.url?.includes(`/api/v1/auth/refresh-token`)) {
+      token = getAccessToken();
+    }
+
+    if (token !== null) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+const getRefreshToken = async () => {
+  try {
+    const token = getAccessToken();
+    if (!token) return null;
+
+    const response = await refreshToken({ accessToken: token });
+
+    return response.accessToken;
+  } catch (e) {
+    return null;
+  }
+};
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const { config } = error;
+
+    if (config.url.includes('refresh-token') || error.response.status !== 401 || config.sent) {
+      return Promise.reject(error);
+    }
+
+    config.sent = true;
+    const accessToken = await getRefreshToken();
+
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+
+    return axios(config);
+  },
+);
 
 export const customInstance = <T>(config: AxiosRequestConfig, options?: AxiosRequestConfig): Promise<T> => {
-  const promise = axios({
+  const promise = instance({
     ...config,
     ...options,
-    baseURL: import.meta.env.VITE_API_BASE_URL,
   }).then(({ data }) => data);
 
   return promise;
