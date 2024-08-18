5e8c95d108982875cbac4743c0910ba2e8b0e241
조예진 <orobos654@gmail.com>
2024-08-18 17:44:10 +0900

    fix: remove node function

--- src/shared/functions/redirectToLoginPage.ts
+++ src/shared/functions/redirectToLoginPage.ts
@@ -1,9 +1,6 @@
-import { redirect } from '@remix-run/node';
-
 export const redirectToLoginPage = () => {
   if (typeof location !== 'undefined') {
     location.href = '/login';
     return;
   }
-  redirect('/login');
 };
