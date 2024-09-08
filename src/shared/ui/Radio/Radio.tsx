04d83fa7031204c7dd85f7f3ba2e1fdbcdd612fe
조예진 <orobos654@gmail.com>
2024-09-08 16:09:01 +0900

    style: use svg for radio

--- src/shared/ui/Radio/Radio.tsx
+++ src/shared/ui/Radio/Radio.tsx
@@ -1,5 +1,6 @@
 import styles from './Radio.module.css';
 import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import { CheckedRadio, UncheckedRadio } from 'src/shared/ui/icons';
 
 export type RadioProps = Exclude<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'type'> & {
   label: string;
@@ -9,6 +10,7 @@ export const Radio = ({ className, label, ...props }: RadioProps) => {
   return (
     <label className={`${styles.Label} ${className}`}>
       <input className={styles.Radio} type={'radio'} {...props} />
+      {props.checked ? <CheckedRadio color={'#CDCACE'} /> : <UncheckedRadio color={'#CDCACE'} />}
       {label}
     </label>
   );
