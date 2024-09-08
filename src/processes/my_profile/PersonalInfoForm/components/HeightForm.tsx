4dbbd75b74342e971bf6894289f9e503d34daa6a
조예진 <orobos654@gmail.com>
2024-09-08 19:53:45 +0900

    fix: do not allow invalid height

--- src/processes/my_profile/PersonalInfoForm/components/HeightForm.tsx
+++ src/processes/my_profile/PersonalInfoForm/components/HeightForm.tsx
@@ -1,10 +1,20 @@
 import styles from 'src/processes/my_profile/PersonalInfoForm/PersonalInfoForm.module.css';
 import { Input } from 'src/shared/ui/Input/Input';
 import { useMyProfileStore } from 'src/entities/profile/model/myProfileStore';
+import { ChangeEvent } from 'react';
 
 export const HeightForm = () => {
   const height = useMyProfileStore((state) => state.height);
   const setHeight = useMyProfileStore((state) => state.setHeight);
+
+  const onChangeHeight = (e: ChangeEvent<HTMLInputElement>) => {
+    const rawValue = e.target.value;
+    const value = Number(rawValue);
+    if (rawValue !== '' && (isNaN(value) || value < 0)) {
+      return;
+    }
+    setHeight(value);
+  };
 
   return (
     <label className={styles.Label}>
@@ -12,9 +22,8 @@ export const HeightForm = () => {
       <Input
         suffixSlot={'cm'}
         placeholder={'자신의 키를 입력해주세요.'}
-        value={height ?? ''}
-        type={'number'}
-        onChange={(e) => setHeight(Number(e.target.value) ?? '')}
+        value={height || ''}
+        onChange={onChangeHeight}
       />
     </label>
   );
