6fc4f53ae5cfd51667dfc997436a079639007ea3
조예진 <orobos654@gmail.com>
2024-09-08 16:09:57 +0900

    feat: add icon components

---
+++ src/shared/ui/icons/UncheckedRadio.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgUncheckedRadio = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="#CDCACE"
+      fillRule="evenodd"
+      d="M12 19a7 7 0 1 0 0-14 7 7 0 0 0 0 14m0 2a9 9 0 1 0 0-18 9 9 0 0 0 0 18"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgUncheckedRadio;
