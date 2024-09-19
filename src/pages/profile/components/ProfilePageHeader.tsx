d07ab57997dd7496238d7e6e860669072899e095
조예진 <orobos654@gmail.com>
2024-09-19 22:21:26 +0900

    feat: add delete info modal

--- src/pages/profile/components/ProfilePageHeader.tsx
+++ src/pages/profile/components/ProfilePageHeader.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@remix-run/react';
+import { Link, useNavigate } from '@remix-run/react';
 import { ArrowLeft, Delete, Edit, Menu, Share } from 'src/shared/ui/icons';
 import { Theme } from 'src/shared/styles/constants';
 import styles from 'src/pages/profile/ProfilePage.module.css';
@@ -10,6 +10,10 @@ import { MyProfile } from 'src/entities/profile/model/myProfileStore';
 import { useTranslation } from 'react-i18next';
 import { ProfileShareBottomSheet } from 'src/features/ProfileShare/ProfileShareBottomSheet';
 import { calculateAge, convertDateObjectToDate } from 'src/shared/vo/date';
+import { Modal } from 'src/shared/ui/Modal/Modal';
+import { useMutation } from '@tanstack/react-query';
+import { deleteInfo } from 'src/types';
+import toast from 'react-hot-toast';
 
 export const ProfilePageHeader = ({
   profile,
@@ -20,10 +24,22 @@ export const ProfilePageHeader = ({
   profile: MyProfile;
   showTitle: boolean;
 }) => {
+  const navigate = useNavigate();
   const { t } = useTranslation();
   const [isShareOpen, setShareOpen] = useState(false);
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
 
   const age = calculateAge(convertDateObjectToDate(profile.birthDate));
+
+  const { mutateAsync: deleteMutation } = useMutation({
+    mutationFn: deleteInfo,
+  });
+
+  const onClickDelete = async () => {
+    await deleteMutation(infoId);
+    navigate('/');
+    toast.success('정보가 삭제되었습니다.', { icon: null });
+  };
 
   return (
     <>
@@ -62,6 +78,7 @@ export const ProfilePageHeader = ({
                     color={'neutral'}
                     prefixSlot={<Delete color={Theme.color.neutral60} />}
                     className={styles.DeleteButton}
+                    onClick={() => setDeleteModalOpen(true)}
                   >
                     삭제하기
                   </Button>
@@ -80,6 +97,24 @@ export const ProfilePageHeader = ({
         )}
       </Header>
       <ProfileShareBottomSheet isOpen={isShareOpen} onClose={() => setShareOpen(false)} infoId={infoId} />
+      <Modal
+        wrapperClassName={styles.DeleteModal}
+        showModal={isDeleteModalOpen}
+        closeModal={() => setDeleteModalOpen(false)}
+      >
+        <div className={styles.DeleteModalBody}>
+          <h2>{profile.name}님의 정보를 삭제할까요?</h2>
+          <p>삭제한 이후 복구할 수 없습니다.</p>
+        </div>
+        <div className={styles.DeleteModalFooter}>
+          <Button variant={'outline'} widthType={'fill'} color={'neutral'} onClick={() => setDeleteModalOpen(false)}>
+            취소
+          </Button>
+          <Button variant={'filled'} widthType={'fill'} color={'primary'} onClick={onClickDelete}>
+            삭제
+          </Button>
+        </div>
+      </Modal>
     </>
   );
 };
