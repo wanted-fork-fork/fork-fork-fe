2b23015b70f7a064564f6da2f298e815e0fa1105
조예진 <orobos654@gmail.com>
2024-09-22 22:40:01 +0900

    fix: upload image one at a time

--- src/pages/form/complete/UploadLoadingPage.tsx
+++ src/pages/form/complete/UploadLoadingPage.tsx
@@ -29,18 +29,20 @@ export const UploadLoadingPage = ({
     mutationFn: uploadImage,
   });
 
-  const uploadFileList = async (files: File[], onUpload: () => void) =>
-    (
-      await Promise.allSettled(
-        files.map(async (file) => {
-          const result = await uploadMutation({ image: file });
-          onUpload();
-          return result;
-        }),
-      )
-    )
-      .map((result) => (result.status === 'fulfilled' ? result.value.data : null))
-      .filter(Boolean) as ImageDto[];
+  const uploadFileList = async (files: File[], onUpload: () => void) => {
+    const results: ImageDto[] = [];
+    // 한번에 너무 많이 요청하지 않도록 하나씩 업로드 요청
+    for (const file of files) {
+      try {
+        const result = await uploadMutation({ image: file });
+        onUpload();
+        results.push(result.data);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    return results;
+  };
 
   const [progress, setProgress] = useState<number>(0);
 
