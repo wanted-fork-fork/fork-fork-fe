86ce22b8b881b54747fca800f93989010fb952f9
조예진 <orobos654@gmail.com>
2024-10-01 19:35:24 +0900

    feat: add link to 개인정보처리방침

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -20,7 +20,7 @@ export const MyPage = ({ userInfo }: { userInfo: UserInfoResponse }) => {
       </div>
       <p className={`label ${styles.Label}`}>약관 및 정책</p>
       <div className={styles.ButtonWrapper}>
-        <Link to={'/'}>
+        <Link to={'https://low-cook-e1a.notion.site/GOOGOO-4ce578de17ff4a4c96430bf2b9e2a100?pvs=4'}>
           <Button color={'neutral'} variant={'ghost'} widthType={'fill'} suffixSlot={<ArrowRight />} textAlign={'left'}>
             개인정보처리방침
           </Button>
