bc15d9a455b1312dcf94a3986feb19c520873992
조예진 <orobos654@gmail.com>
2024-10-03 10:20:22 +0900

    fix: do not send authorization key in share page

--- src/app/routes/share.$key.tsx
+++ src/app/routes/share.$key.tsx
@@ -1,11 +1,9 @@
 import { json, LoaderFunction, MetaFunction } from '@remix-run/node';
-import { authenticate } from '../server/authenticate';
 import { getInfoBySharingId } from '../../types';
 import { useLoaderData } from '@remix-run/react';
 import { useMemo } from 'react';
 import { convertDtoToProfile } from '../../entities/profile/model/convertProfileToDto';
 import { MyProfileProvider } from '../../entities/profile/model/myProfileStore';
-import { commitSession } from 'src/app/server/sessions';
 import { SharedProfilePage } from 'src/pages/shared_profile/SharedProfilePage';
 import { getNickname } from 'src/entities/profile/lib/getNickname';
 
@@ -19,9 +17,7 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const loader: LoaderFunction = async ({ request, params }) => {
-  const { accessToken, newSession } = await authenticate(request);
-
+export const loader: LoaderFunction = async ({ params }) => {
   const { key } = params;
 
   if (!key) {
@@ -31,20 +27,9 @@ export const loader: LoaderFunction = async ({ request, params }) => {
     });
   }
 
-  const { data } = await getInfoBySharingId(key, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
+  const { data } = await getInfoBySharingId(key);
 
-  return json(
-    { profile: data, key },
-    {
-      headers: {
-        ...(newSession && { 'Set-Cookie': await commitSession(newSession) }),
-      },
-    },
-  );
+  return json({ profile: data, key });
 };
 
 export default function Page() {
