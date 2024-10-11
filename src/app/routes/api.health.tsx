112666a6fb7d8b54cd627cdf47021844b7b4d6c1
조예진 <orobos654@gmail.com>
2024-10-11 21:05:31 +0900

    chore: add endpoint to health-check

---
+++ src/app/routes/api.health.tsx
@@ -0,0 +1,12 @@
+import { LoaderFunction } from '@remix-run/node';
+
+const apiURL = new URL(process.env.API_BASE_URL ?? '');
+
+export const loader: LoaderFunction = async (args) => {
+  const url = new URL(args.request.url);
+  url.protocol = apiURL.protocol;
+  url.host = apiURL.host;
+  url.port = apiURL.port;
+
+  return fetch(url.toString(), new Request(args.request));
+};
