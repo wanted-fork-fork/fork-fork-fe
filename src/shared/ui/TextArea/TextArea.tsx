e15d085286fca746ca4c994a0674b8e4f9466609
조예진 <orobos654@gmail.com>
2024-10-13 22:57:37 +0900

    feat: show text length in textArea

--- src/shared/ui/TextArea/TextArea.tsx
+++ src/shared/ui/TextArea/TextArea.tsx
@@ -4,9 +4,15 @@ import { DetailedHTMLProps, TextareaHTMLAttributes } from 'react';
 type TextAreaProps = DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>;
 
 export const TextArea = ({ ...props }: TextAreaProps) => {
+  const length = typeof props.value === 'string' ? props.value.length : 0;
   return (
     <div className={styles.Container}>
       <textarea className={styles.TextArea} {...props} />
+      {props.maxLength && (
+        <div className={styles.TextLength}>
+          {length ?? 0}/{props.maxLength}
+        </div>
+      )}
     </div>
   );
 };
