6c21ab3992263fc8c8cfdca9cbd321cf7893c41a
조예진 <orobos654@gmail.com>
2024-10-26 14:58:45 +0900

    fix: do not use test jwt

--- src/app/routes/auth.kakao.tsx
+++ src/app/routes/auth.kakao.tsx
@@ -3,19 +3,6 @@ import { loginKakao } from 'src/types';
 import { commitSession, generateExpiredDate, getAuthSession } from 'src/app/server/sessions';
 
 export const loader: LoaderFunction = async ({ request }) => {
-  if (import.meta.env.DEV) {
-    const session = await getAuthSession(request);
-    session.set('accessToken', process.env.VITE_DEV_JWT_TOKEN ?? '');
-    session.set('refreshToken', process.env.VITE_DEV_JWT_TOKEN ?? '');
-    session.set('expiredAt', generateExpiredDate());
-
-    return redirect('/', {
-      headers: {
-        'Set-Cookie': await commitSession(session),
-      },
-    });
-  }
-
   const searchParams = new URL(request.url).searchParams;
   const code = searchParams.get('code');
 
