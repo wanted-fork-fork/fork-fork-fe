33d2e0be2cf4dfe3bfbb939f35e44729482338ba
조예진 <orobos654@gmail.com>
2024-11-10 13:14:35 +0900

    refactor: use cva

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,8 +1,9 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
 import styles from './Button.module.css';
+import { cva } from 'cva';
 
 type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
-  variant: 'filled' | 'outline' | 'ghost' | 'link';
+  variant: 'filled' | 'outline' | 'ghost';
   color: 'primary' | 'neutral';
   widthType: 'fill' | 'hug';
   suffixSlot?: ReactNode;
@@ -10,6 +11,26 @@ type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HT
   textAlign?: 'left' | 'center' | 'right';
   size?: 'fit' | 'S' | 'M';
 };
+
+const buttonStyle = cva(styles.Button, {
+  variants: {
+    disabled: { enabled: '', disabled: '' },
+    widthType: { fill: styles.WidthType_Fill, hug: styles.WidthType_Hug },
+    textAlign: { left: styles.TextAlign_Left, center: styles.TextAlign_Center, right: styles.TextAlign_Right },
+    size: { M: styles.Size_M, S: styles.Size_S, fit: styles.Size_Fit },
+    variant: { filled: '', outline: '', ghost: '' },
+    color: { primary: '', neutral: '' },
+  },
+  compoundVariants: [
+    { variant: 'filled', color: 'primary', className: styles.Variant_Filled_Primary },
+    { variant: 'filled', color: 'neutral', className: styles.Variant_Filled_Neutral },
+    { variant: 'outline', color: 'primary', className: styles.Variant_Outline_Primary },
+    { variant: 'outline', color: 'neutral', className: styles.Variant_Outline_Neutral },
+    { variant: 'ghost', color: 'primary', className: styles.Variant_Ghost_Primary },
+    { variant: 'ghost', color: 'neutral', className: styles.Variant_Ghost_Neutral },
+    { variant: 'ghost', disabled: 'disabled', className: styles.Disabled_Ghost },
+  ],
+});
 
 export const Button = ({
   className = '',
@@ -24,13 +45,8 @@ export const Button = ({
   ...props
 }: ButtonProps) => (
   <button
-    className={`${styles.Button} ${className}`}
+    className={`${buttonStyle({ variant, color, widthType, textAlign, size, disabled: props.disabled ? 'disabled' : 'enabled' })} ${className}`}
     {...props}
-    data-variant={variant}
-    data-color={color}
-    data-size={size}
-    data-width-type={widthType}
-    data-text-align={textAlign}
   >
     {prefixSlot && <span className={styles.PrefixSlot}>{prefixSlot}</span>}
     {children && <span className={styles.Center}>{children}</span>}
