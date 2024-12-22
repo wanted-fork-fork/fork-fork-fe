da4be841662e24a947dedb868826f214001d8ea0
조예진 <orobos654@gmail.com>
2024-12-22 18:54:31 +0900

    fix: fix type name

--- src/processes/ideal_partner/DrinkingForm/DrinkingForm.tsx
+++ src/processes/ideal_partner/DrinkingForm/DrinkingForm.tsx
@@ -1,13 +1,13 @@
 import styles from './DrinkingForm.module.css';
 import { RadioList, RadioMeta } from 'src/shared/ui/RadioList/RadioList';
 import { useIdealPartnerStore } from 'src/entities/ideal_partner/model/idealPartnerStore';
-import { DrinkingDrinkingCategory } from 'src/types';
 import { useTranslation } from 'react-i18next';
 import { useMemo } from 'react';
 import { DistributedOmit } from '../../../shared/types/distributedOmit';
 import { useIdealPartnerFormProcessStore } from '../_store/idealPartnerFormProcessStore';
+import { IdealPartnerDrinkingDrinkingCategory } from 'src/types';
 
-const drinkingRadioMeta: DistributedOmit<RadioMeta<DrinkingDrinkingCategory>, 'name'>[] = [
+const drinkingRadioMeta: DistributedOmit<RadioMeta<IdealPartnerDrinkingDrinkingCategory>, 'name'>[] = [
   { key: 'NO_PROBLEM', allowInput: false },
   { key: 'ONE_TWO_TIMES_A_WEEK', allowInput: false },
   { key: 'ONE_TWO_TIMES_A_MONTH', allowInput: false },
@@ -24,7 +24,7 @@ export const DrinkingForm = () => {
   const addTouchedStep = useIdealPartnerFormProcessStore((state) => state.addTouchedStep);
   const touchedSteps = useIdealPartnerFormProcessStore((state) => state.touchedSteps);
 
-  const onSelect = (category: DrinkingDrinkingCategory) => {
+  const onSelect = (category: IdealPartnerDrinkingDrinkingCategory) => {
     setDrinkingCategory(category);
     addTouchedStep('IDEAL_DRINKING');
   };
