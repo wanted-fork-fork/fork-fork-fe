7693b8a28d50466cd2fbd9d68fbb13a06a6ac815
조예진 <orobos654@gmail.com>
2024-12-22 18:46:10 +0900

    feat: use radio in ideal partner hobby form

--- src/processes/ideal_partner/HobbyForm/HobbyForm.tsx
+++ src/processes/ideal_partner/HobbyForm/HobbyForm.tsx
@@ -1,24 +1,49 @@
 import styles from './HobbyForm.module.css';
 import { useIdealPartnerStore } from 'src/entities/ideal_partner/model/idealPartnerStore';
-import { ChipList } from 'src/shared/ui/ChipList/ChipList';
-import { ExampleHobbyList } from 'src/entities/hobby/constants/hobbies';
+import { RadioList, RadioMeta } from 'src/shared/ui/RadioList/RadioList';
+import { IdealPartnerRequestHobbies } from 'src/types';
+import { useIdealPartnerFormProcessStore } from 'src/processes/ideal_partner/_store/idealPartnerFormProcessStore';
+import { useProfileFirstName } from 'src/entities/profile/lib/useProfileFirstName';
+import { useMyProfileStore } from 'src/entities/profile/model/myProfileStore';
+import { Chip } from 'src/shared/ui/Chip/Chip';
+
+const hobbyRadioMeta: RadioMeta<IdealPartnerRequestHobbies>[] = [
+  { key: 'IMPORTANT', allowInput: false, name: '네, 취미 생활을 같이 하고싶어요.' },
+  { key: 'NOT_IMPORTANT', allowInput: false, name: '상관없어요' },
+];
 
 export const HobbyForm = () => {
+  const name = useProfileFirstName();
+  const selectedHobbies = useMyProfileStore((state) => state.hobbies);
+
   const hobbies = useIdealPartnerStore((state) => state.hobbies);
   const setHobbies = useIdealPartnerStore((state) => state.setHobbies);
+  const touchedSteps = useIdealPartnerFormProcessStore((state) => state.touchedSteps);
+  const addTouchedStep = useIdealPartnerFormProcessStore((state) => state.addTouchedStep);
+
+  const onSelect = (category: IdealPartnerRequestHobbies) => {
+    setHobbies(category);
+    addTouchedStep('IDEAL_HOBBY');
+  };
 
   return (
     <>
       <section className={styles.Container}>
-        <ChipList
-          defaultList={ExampleHobbyList}
-          selectedList={hobbies}
-          setSelectedList={setHobbies}
-          customInputTitle={'추가하실 취미를 입력해주세요.'}
-          customInputPlaceholder={'취미 입력'}
-          makeItem={(name) => ({ name })}
-          hasItem={(list, target) => list.some((item) => item.name === target.name)}
-        />
+        <div>
+          <small className={styles.Description}>{name}님이 선택하신 취미</small>
+          <div className={styles.ChipWrapper}>
+            {selectedHobbies.map((h) => (
+              <Chip key={h.name}>{h.name}</Chip>
+            ))}
+          </div>
+        </div>
+        <div>
+          <RadioList
+            radioMetaList={hobbyRadioMeta}
+            selected={touchedSteps.has('IDEAL_HOBBY') ? hobbies : null}
+            onSelect={onSelect}
+          />
+        </div>
       </section>
     </>
   );
