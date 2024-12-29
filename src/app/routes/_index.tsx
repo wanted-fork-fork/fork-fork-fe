fbcd1bdd2464ad05644c699df715f2a4e383cf25
조예진 <orobos654@gmail.com>
2024-12-29 18:35:44 +0100

    feat: show onboarding to new user

--- src/app/routes/_index.tsx
+++ src/app/routes/_index.tsx
@@ -1,11 +1,13 @@
 import type { LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { authenticate } from 'src/app/server/authenticate';
-import { getAllInfo, info } from 'src/types';
+import { getAllInfo, hasSeenOnboarding, info } from 'src/types';
 import { InfoListPage } from 'src/pages/main/info_list/InfoListPage';
 import { useLoaderData } from '@remix-run/react';
 import { GenerateFormLink } from 'src/widgets/GenerateFormLink/GenerateFormLink';
 import { commitSession } from 'src/app/server/sessions';
+import { OnboardingPage } from 'src/pages/main/onboarding/OnboardingPage';
+import { useState } from 'react';
 
 export const loader: LoaderFunction = async ({ request }) => {
   const { accessToken, newSession } = await authenticate(request);
@@ -22,8 +24,14 @@ export const loader: LoaderFunction = async ({ request }) => {
     },
   });
 
+  const { data: seenOnboarding } = await hasSeenOnboarding({
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
   return json(
-    { userInfo, profileList },
+    { userInfo, profileList, seenOnboarding },
     {
       headers: {
         ...(newSession && { 'Set-Cookie': await commitSession(newSession) }),
@@ -33,11 +41,16 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Index() {
-  const { profileList, userInfo } = useLoaderData<typeof loader>();
-  return (
+  const { profileList, userInfo, seenOnboarding } = useLoaderData<typeof loader>();
+
+  const [seenOnboardingState, setSeenOnboardingState] = useState(seenOnboarding);
+
+  return seenOnboardingState ? (
     <>
       <InfoListPage userInfo={userInfo} profileList={profileList} />
       <GenerateFormLink />
     </>
+  ) : (
+    <OnboardingPage onClickNextPage={() => setSeenOnboardingState(true)} />
   );
 }
