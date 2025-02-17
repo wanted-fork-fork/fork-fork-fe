e97f5b89e8ffcb815a1ccf4afb75e3bdf5504085
조예진 <orobos654@gmail.com>
2025-02-18 02:08:57 +0900

    style: add disabled styles

--- src/widgets/GenerateFormLink/GenerateFormLinkBottomSheet.tsx
+++ src/widgets/GenerateFormLink/GenerateFormLinkBottomSheet.tsx
@@ -56,12 +56,26 @@ const GenerateFormBottomSheetContent = () => {
         <small className={styles.Description}>소개를 받고싶어 하는 지인의 정보를 저장하세요.</small>
       </div>
       <div className={styles.ButtonWrapper}>
-        <IconBoxButton icon={<Link />} text={'링크 복사'} onClick={onClickCopyLink} />
         <IconBoxButton
-          icon={<img src="/images/kakao.png" alt="카카오톡으로 공유하기" width={29} height={29} />}
-          iconBackgroundColor={Theme.color.kakao}
+          icon={<Link className={styles.LinkIcon} color={'transparent'} data-disabled={!isOpen} />}
+          text={'링크 복사'}
+          onClick={onClickCopyLink}
+          disabled={!isOpen}
+          iconBackgroundColor={isOpen ? Theme.color.neutral90 : Theme.color.neutral10}
+        />
+        <IconBoxButton
+          icon={
+            <img
+              src={isOpen ? '/images/kakao.png' : '/images/kakao_disabled.png'}
+              alt="카카오톡으로 공유하기"
+              width={29}
+              height={29}
+            />
+          }
+          iconBackgroundColor={isOpen ? Theme.color.kakao : Theme.color.neutral10}
           text={'카카오톡 공유'}
           onClick={onClickShareKakao}
+          disabled={!isOpen}
         />
       </div>
       <div className={styles.LinkConfigSection}>
@@ -83,7 +97,7 @@ const GenerateFormBottomSheetContent = () => {
             <p>기존 링크와 다른 주소로 새로운 링크가 생성됩니다.</p>
           </div>
           <Button variant={'ghost'} widthType={'hug'} color={'primary'} size={'fit'} onClick={onClickRegenerate}>
-            <Refresh color={Theme.color.primary} />
+            <Refresh color={isOpen ? Theme.color.primary : Theme.color.neutral30} />
           </Button>
         </div>
       </div>
