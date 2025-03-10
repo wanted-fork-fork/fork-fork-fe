d18b7d099cb1bd186786cd428fee4bf75d187a96
조예진 <orobos654@gmail.com>
2025-03-10 19:53:12 +0900

    feat: show email in noti config page

--- src/pages/mypage/notification/NotificationConfigPage.tsx
+++ src/pages/mypage/notification/NotificationConfigPage.tsx
@@ -4,8 +4,15 @@ import { ArrowLeft } from 'src/shared/ui/icons';
 import { Toggle } from 'src/shared/ui/Toggle/Toggle';
 import { useState } from 'react';
 import styles from './NotificationConfigPage.module.css';
+import { Button } from 'src/shared/ui/Button/Button';
 
-export const NotificationConfigPage = ({ allowEmailNotification }: { allowEmailNotification: boolean }) => {
+export const NotificationConfigPage = ({
+  email,
+  allowEmailNotification,
+}: {
+  email?: string;
+  allowEmailNotification: boolean;
+}) => {
   const [emailNoti, setEmailNoti] = useState(allowEmailNotification);
   const navigate = useNavigate();
   const handleToggle = (value: boolean) => {
@@ -30,10 +37,23 @@ export const NotificationConfigPage = ({ allowEmailNotification }: { allowEmailN
         후보자 추가 알림 받기
       </Header>
       <div className={styles.Body}>
-        <div className={styles.Row}>
+        <div className={`${styles.NotiConfigRow} ${styles.Row}`}>
           <span className={styles.Label}>알림 받기</span>
           <Toggle checked={emailNoti} onToggle={handleToggle} />
         </div>
+        {email && (
+          <div className={styles.EmailConfigRow}>
+            <div className={styles.Row}>
+              <span className={styles.Label}>메일 설정</span>
+              <Link to={'/account/email/edit'}>
+                <Button widthType={'hug'} size={'fit'} variant={'ghost'}>
+                  변경
+                </Button>
+              </Link>
+            </div>
+            <p>{email}</p>
+          </div>
+        )}
       </div>
     </>
   );
