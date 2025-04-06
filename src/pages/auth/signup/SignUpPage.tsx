ce641bab820b7aafc9a091e1fba75a11b9a22e6c
조예진 <orobos654@gmail.com>
2025-04-06 19:50:44 +0900

    feat: add SignUpCompletePage

--- src/pages/auth/signup/SignUpPage.tsx
+++ src/pages/auth/signup/SignUpPage.tsx
@@ -6,6 +6,9 @@ import { NameForm } from 'src/processes/signup/NameForm/NameForm';
 import { useStep } from 'src/shared/functions/useStep';
 import { useReducer } from 'react';
 import { useNavigate } from '@remix-run/react';
+import { useMutation } from '@tanstack/react-query';
+import { signup } from 'src/types';
+import { SignUpCompletePage } from 'src/processes/signup/SignUpCompletePage/SignUpCompletePage';
 
 type SignUpState = {
   email: string;
@@ -38,45 +41,49 @@ export const SignUpPage = () => {
   const [state, dispatch] = useReducer(reducer, { email: '', password: '', name: '' });
   const { step, increase, decrease } = useStep({ max: 3, initialValue: 0 });
 
+  const { mutate, isPending, data, error } = useMutation({
+    mutationFn: signup,
+  });
+
   return (
     <FormLayout.Container>
       <FormLayout.Header onPrev={step === 0 ? () => navigate('/login/email') : decrease} />
-      <SwitchCase
-        value={step}
-        caseBy={{
-          0: (
-            <EmailForm
-              onSubmitEmail={(email) => {
-                dispatch({ type: 'SET_EMAIL', payload: { email } });
-                increase();
-              }}
-            />
-          ),
-          1: (
-            <PasswordForm
-              password={state.password}
-              onSubmit={(password) => {
-                dispatch({ type: 'SET_PASSWORD', payload: { password } });
-                increase();
-              }}
-            />
-          ),
-          2: (
-            <NameForm
-              name={state.name}
-              onSubmit={(name) => {
-                dispatch({ type: 'SET_NAME', payload: { name } });
-                increase();
-              }}
-            />
-          ),
-          3: (
-            <>
-              email: {state.email} pass: {state.password} name: {state.name}
-            </>
-          ),
-        }}
-      />
+      {!(data || error) && (
+        <SwitchCase
+          value={step}
+          caseBy={{
+            0: (
+              <EmailForm
+                onSubmitEmail={(email) => {
+                  dispatch({ type: 'SET_EMAIL', payload: { email } });
+                  increase();
+                }}
+              />
+            ),
+            1: (
+              <PasswordForm
+                password={state.password}
+                onSubmit={(password) => {
+                  dispatch({ type: 'SET_PASSWORD', payload: { password } });
+                  increase();
+                }}
+              />
+            ),
+            2: (
+              <NameForm
+                name={state.name}
+                isLoading={isPending}
+                onSubmit={(name) => {
+                  dispatch({ type: 'SET_NAME', payload: { name } });
+                  mutate({ ...state });
+                }}
+              />
+            ),
+          }}
+        />
+      )}
+      {data && <SignUpCompletePage name={state.name} />}
+      {error && '에러'}
     </FormLayout.Container>
   );
 };
---
+++ src/processes/signup/SignUpCompletePage/SignUpCompletePage.tsx
@@ -0,0 +1,26 @@
+import { Button } from 'src/shared/ui/Button/Button';
+import { Link } from '@remix-run/react';
+import { FormLayout } from 'src/pages/layout/FormLayout';
+import styles from './SignUpCompletePage.module.css';
+
+export const SignUpCompletePage = ({ name }: { name: string }) => {
+  return (
+    <FormLayout.Container>
+      <FormLayout.Body className={styles.Body}>
+        <h2>
+          반갑습니다, {name}님!
+          <br />
+          <br />
+          구구가 소개팅을 주선하는 <br />
+          모든 과정을 간편하게 도와드릴게요.
+        </h2>
+        <img src="/images/signup_complete.png" alt="회원가입 완료" />
+      </FormLayout.Body>
+      <FormLayout.Footer>
+        <Link to={'/login/email'}>
+          <Button widthType={'fill'}>로그인하고 시작하기</Button>
+        </Link>
+      </FormLayout.Footer>
+    </FormLayout.Container>
+  );
+};
