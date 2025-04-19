8677a3dac2f80b941b5fc777b8b51ffc3898342a
조예진 <orobos654@gmail.com>
2025-04-20 00:32:04 +0900

    fix: add story args

--- src/pages/main/info_list/InfoListPage.stories.ts
+++ src/pages/main/info_list/InfoListPage.stories.ts
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { InfoListPage } from 'src/pages/main/info_list/InfoListPage';
+import { profileMock } from 'src/entities/profile/api/__mock__/profile.mock';
 
 const meta: Meta<typeof InfoListPage> = {
   component: InfoListPage,
@@ -9,5 +10,13 @@ export default meta;
 type Story = StoryObj<typeof InfoListPage>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    userInfo: {
+      name: '김구구',
+      userId: '123',
+      email: 'orobos654@gmail.com',
+      receiveEmail: false,
+    },
+    profileList: Array.from({ length: 10 }).map(() => profileMock),
+  },
 };
