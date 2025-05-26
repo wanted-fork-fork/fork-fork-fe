6d956c8a5f656d07820263e9fedca63f7987010f
조예진 <orobos654@gmail.com>
2025-05-26 20:18:01 +0900

    fix: 마이페이지 스크롤 추가

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -10,6 +10,8 @@ import { useState } from 'react';
 import { OnboardingPage } from 'src/pages/main/onboarding_coachmark/OnboardingPage';
 import { IconButton } from 'src/shared/ui/IconButton/IconButton';
 import { SwitchCase } from 'src/shared/ui/SwitchCase';
+import { ScrollView } from 'src/shared/ui/ScrollView/ScrollView';
+import { FormLayout } from 'src/pages/layout/FormLayout';
 
 const ArrowIcon = () => <ArrowRight color={Theme.color.neutral40} />;
 
@@ -22,133 +24,135 @@ export const MyPage = ({ userInfo }: { userInfo: UserInfoResponse }) => {
       onEndOnboarding={() => setShowTutorial(false)}
     />
   ) : (
-    <>
+    <FormLayout.Container>
       <div className={styles.Header}>
         <Link to={'/'}>
           <Close />
         </Link>
         <h2>마이페이지</h2>
       </div>
-      <Link to={'/mypage/edit'}>
-        <div className={styles.UserInfoWrapper}>
+      <ScrollView viewportClassName={styles.Body}>
+        <Link to={'/mypage/edit'}>
+          <div className={styles.UserInfoWrapper}>
+            <div>
+              <UserAvatar size={72} imageSrc={userInfo.profileImage} />
+              <SwitchCase
+                value={userInfo.joinType}
+                caseBy={{
+                  KAKAO: (
+                    <div className={styles.LoginMethod} style={{ backgroundColor: Theme.color.kakao }}>
+                      <img src={'/images/kakao.png'} alt={'카카오 계정으로 로그인한 계정입니다'} />
+                    </div>
+                  ),
+                  EMAIL: (
+                    <div className={styles.LoginMethod} style={{ backgroundColor: Theme.color.neutral90 }}>
+                      <EmailFilled color={Theme.color.primary} />
+                    </div>
+                  ),
+                }}
+              />
+            </div>
+            <div className={styles.UserInfoNameWrapper}>
+              <h3>{userInfo.name}</h3>
+              {userInfo.email && <p className={styles.Email}>{userInfo.email}</p>}
+            </div>
+            <IconButton className={styles.UserInfoMoreButton}>
+              <ArrowRight width={24} height={24} />
+            </IconButton>
+          </div>
+        </Link>
+        <hr className={styles.Divider} />
+        <div className={styles.MenuList}>
           <div>
-            <UserAvatar size={72} imageSrc={userInfo.profileImage} />
-            <SwitchCase
-              value={userInfo.joinType}
-              caseBy={{
-                KAKAO: (
-                  <div className={styles.LoginMethod} style={{ backgroundColor: Theme.color.kakao }}>
-                    <img src={'/images/kakao.png'} alt={'카카오 계정으로 로그인한 계정입니다'} />
-                  </div>
-                ),
-                EMAIL: (
-                  <div className={styles.LoginMethod} style={{ backgroundColor: Theme.color.neutral90 }}>
-                    <EmailFilled color={Theme.color.primary} />
-                  </div>
-                ),
-              }}
-            />
+            <p className={`label ${styles.Label}`}>구구 소식</p>
+            <div className={styles.ButtonWrapper}>
+              <Link to={NOTICE_URL}>
+                <Button
+                  color={'neutral'}
+                  variant={'ghost'}
+                  widthType={'fill'}
+                  suffixSlot={<ArrowIcon />}
+                  textAlign={'left'}
+                >
+                  공지사항
+                </Button>
+              </Link>
+            </div>
           </div>
-          <div className={styles.UserInfoNameWrapper}>
-            <h3>{userInfo.name}</h3>
-            {userInfo.email && <p className={styles.Email}>{userInfo.email}</p>}
+          <div>
+            <p className={`label ${styles.Label}`}>설정</p>
+            <div className={styles.ButtonWrapper}>
+              <Link to={'/mypage/notification'}>
+                <Button
+                  color={'neutral'}
+                  variant={'ghost'}
+                  widthType={'fill'}
+                  suffixSlot={<ArrowIcon />}
+                  textAlign={'left'}
+                >
+                  후보자 추가 알림 받기
+                </Button>
+              </Link>
+            </div>
           </div>
-          <IconButton className={styles.UserInfoMoreButton}>
-            <ArrowRight width={24} height={24} />
-          </IconButton>
-        </div>
-      </Link>
-      <hr className={styles.Divider} />
-      <div className={styles.MenuList}>
-        <div>
-          <p className={`label ${styles.Label}`}>구구 소식</p>
-          <div className={styles.ButtonWrapper}>
-            <Link to={NOTICE_URL}>
+          <div>
+            <p className={`label ${styles.Label}`}>고객 지원</p>
+            <div className={styles.ButtonWrapper}>
+              <Link to={FAQ_URL}>
+                <Button
+                  color={'neutral'}
+                  variant={'ghost'}
+                  widthType={'fill'}
+                  suffixSlot={<ArrowIcon />}
+                  textAlign={'left'}
+                >
+                  자주 묻는 질문
+                </Button>
+              </Link>
+              <Link to={INQUIRY_URL}>
+                <Button
+                  color={'neutral'}
+                  variant={'ghost'}
+                  widthType={'fill'}
+                  suffixSlot={<ArrowIcon />}
+                  textAlign={'left'}
+                >
+                  문의하기
+                </Button>
+              </Link>
               <Button
                 color={'neutral'}
                 variant={'ghost'}
                 widthType={'fill'}
                 suffixSlot={<ArrowIcon />}
                 textAlign={'left'}
+                onClick={() => setShowTutorial(true)}
               >
-                공지사항
+                튜토리얼 다시보기
               </Button>
+              <Link to={'/mypage/term'}>
+                <Button
+                  color={'neutral'}
+                  variant={'ghost'}
+                  widthType={'fill'}
+                  suffixSlot={<ArrowIcon />}
+                  textAlign={'left'}
+                >
+                  약관 및 정책
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <div className={styles.SmallButtonsWrapper}>
+            <Link to={'/logout'} className={styles.SmallButton}>
+              로그아웃
+            </Link>
+            <Link to={'/account/delete'} className={styles.SmallButton}>
+              회원 탈퇴
             </Link>
           </div>
         </div>
-        <div>
-          <p className={`label ${styles.Label}`}>설정</p>
-          <div className={styles.ButtonWrapper}>
-            <Link to={'/mypage/notification'}>
-              <Button
-                color={'neutral'}
-                variant={'ghost'}
-                widthType={'fill'}
-                suffixSlot={<ArrowIcon />}
-                textAlign={'left'}
-              >
-                후보자 추가 알림 받기
-              </Button>
-            </Link>
-          </div>
-        </div>
-        <div>
-          <p className={`label ${styles.Label}`}>고객 지원</p>
-          <div className={styles.ButtonWrapper}>
-            <Link to={FAQ_URL}>
-              <Button
-                color={'neutral'}
-                variant={'ghost'}
-                widthType={'fill'}
-                suffixSlot={<ArrowIcon />}
-                textAlign={'left'}
-              >
-                자주 묻는 질문
-              </Button>
-            </Link>
-            <Link to={INQUIRY_URL}>
-              <Button
-                color={'neutral'}
-                variant={'ghost'}
-                widthType={'fill'}
-                suffixSlot={<ArrowIcon />}
-                textAlign={'left'}
-              >
-                문의하기
-              </Button>
-            </Link>
-            <Button
-              color={'neutral'}
-              variant={'ghost'}
-              widthType={'fill'}
-              suffixSlot={<ArrowIcon />}
-              textAlign={'left'}
-              onClick={() => setShowTutorial(true)}
-            >
-              튜토리얼 다시보기
-            </Button>
-            <Link to={'/mypage/term'}>
-              <Button
-                color={'neutral'}
-                variant={'ghost'}
-                widthType={'fill'}
-                suffixSlot={<ArrowIcon />}
-                textAlign={'left'}
-              >
-                약관 및 정책
-              </Button>
-            </Link>
-          </div>
-        </div>
-        <div className={styles.SmallButtonsWrapper}>
-          <Link to={'/logout'} className={styles.SmallButton}>
-            로그아웃
-          </Link>
-          <Link to={'/account/delete'} className={styles.SmallButton}>
-            회원 탈퇴
-          </Link>
-        </div>
-      </div>
-    </>
+      </ScrollView>
+    </FormLayout.Container>
   );
 };
