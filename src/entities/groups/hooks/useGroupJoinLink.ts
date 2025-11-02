0cb66802dd079fe8acdb3fca8c4c08594fa9f28f
조예진 <orobos654@gmail.com>
2025-11-03 00:32:31 +0900

    fix: do not post if no group id

--- src/entities/groups/hooks/useGroupJoinLink.ts
+++ src/entities/groups/hooks/useGroupJoinLink.ts
@@ -5,6 +5,7 @@ export const useGroupJoinLink = (groupId: string) => {
   const { data, isLoading } = useQuery({
     queryFn: () => createGroupInviteLink(groupId),
     queryKey: ['group', 'join', groupId],
+    enabled: groupId != null,
   });
 
   const link = data?.data.inviteKey ? `${location.origin}/groups/join/${data.data.inviteKey}` : '';
