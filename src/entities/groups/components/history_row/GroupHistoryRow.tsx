e35c1fb73df2d62ada8112f4a232ba90efb74185
조예진 <orobos654@gmail.com>
2025-11-19 00:33:22 +0900

    fix: edit history response type

--- src/entities/groups/components/history_row/GroupHistoryRow.tsx
+++ src/entities/groups/components/history_row/GroupHistoryRow.tsx
@@ -1,4 +1,3 @@
-import { GroupHistory } from 'src/entities/groups/mocks/groupInfoMock';
 import { Avatar } from 'src/shared/ui/Avatar/Avatar';
 import dayjs from 'dayjs';
 import { ArrowRight } from 'src/shared/ui/icons';
@@ -6,29 +5,29 @@ import Flex from 'src/shared/ui/Flex/Flex';
 import styles from './GroupHistoryRow.module.css';
 import { Theme } from 'src/shared/styles/constants';
 import { Link } from '@remix-run/react';
+import { GroupHistoryResponse, GroupHistoryResponseType } from 'src/types';
 
-export const GroupHistoryRow = ({
-  history: { member, contents, info, date },
-  groupId,
-}: {
-  history: GroupHistory;
-  groupId: string;
-}) => {
+const HISTORY_TYPE_TO_TEXT: Record<GroupHistoryResponseType, string> = {
+  INFO_ADDED: '후보자 공유',
+  INFO_SHARED: '외부 정보 공유',
+};
+
+export const GroupHistoryRow = ({ history, groupId }: { history: GroupHistoryResponse; groupId: string }) => {
   return (
     <Flex className={styles.Container} gap={12} direction={'vertical'}>
       <div className={styles.HistoryInfo}>
-        <Avatar fallback={''} shape={'circle'} size={40} src={member.profileImage} />
+        <Avatar fallback={''} shape={'circle'} size={40} src={history.userProfileImage} />
         <div className={styles.Contents}>
-          <p>{contents}</p>
+          <p>{HISTORY_TYPE_TO_TEXT[history.type]}</p>
           <div className={styles.Info}>
-            <span>{member.name}</span>
-            <span>{dayjs(date).format('YYYY. MM. DD HH:mm')}</span>
+            <span>{history.userName}</span>
+            <span>{dayjs(history.timestamp).format('YYYY. MM. DD HH:mm')}</span>
           </div>
         </div>
       </div>
-      <Link className={styles.ProfileInfo} to={`/group/${groupId}/profile/${info.id}`}>
-        <Avatar fallback={''} shape={'circle'} size={24} src={info.images[0].url} />
-        <span className={styles.Name}>{info.name}</span>
+      <Link className={styles.ProfileInfo} to={`/groups/${groupId}/profiles/${history.infoId}`}>
+        <Avatar fallback={''} shape={'circle'} size={24} src={history.userProfileImage} />
+        <span className={styles.Name}>{history.userName}</span>
         <ArrowRight width={16} color={Theme.color.neutral40} />
       </Link>
     </Flex>
--- src/pages/groups/history/GroupHistoryPage.tsx
+++ src/pages/groups/history/GroupHistoryPage.tsx
@@ -3,13 +3,19 @@ import { Link } from '@remix-run/react';
 import { ArrowLeft } from 'src/shared/ui/icons';
 import { Theme } from 'src/shared/styles/constants';
 import { GroupHistoryRow } from 'src/entities/groups/components/history_row/GroupHistoryRow';
-import { GroupHistory } from 'src/entities/groups/mocks/groupInfoMock';
 import { ScrollView } from 'src/shared/ui/ScrollView/ScrollView';
 import Flex from 'src/shared/ui/Flex/Flex';
 import styles from './GroupHistoryPage.module.css';
 import { Empty } from 'src/shared/ui/Empty/Empty';
+import { GroupHistoryResponse } from 'src/types';
 
-export const GroupHistoryPage = ({ groupId, historyList }: { groupId: string; historyList: GroupHistory[] }) => {
+export const GroupHistoryPage = ({
+  groupId,
+  historyList,
+}: {
+  groupId: string;
+  historyList: GroupHistoryResponse[];
+}) => {
   return (
     <div className={styles.Container}>
       <Header
@@ -24,7 +30,7 @@ export const GroupHistoryPage = ({ groupId, historyList }: { groupId: string; hi
       <ScrollView>
         <Flex gap={8} direction={'vertical'}>
           {historyList.map((history) => (
-            <GroupHistoryRow key={history.id} groupId={groupId} history={history} />
+            <GroupHistoryRow key={history.timestamp} groupId={groupId} history={history} />
           ))}
           {historyList.length === 0 && <Empty text={'표시할 히스토리가 없습니다.'} />}
         </Flex>
