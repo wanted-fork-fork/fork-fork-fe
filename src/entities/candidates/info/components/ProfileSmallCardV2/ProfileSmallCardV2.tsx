6e857cade78d1d579c732a0da5b46845577762c6
조예진 <orobos654@gmail.com>
2025-12-15 01:30:46 +0900

    feat: add status chip to small profile card

--- src/entities/candidates/info/components/ProfileSmallCardV2/ProfileSmallCardV2.tsx
+++ src/entities/candidates/info/components/ProfileSmallCardV2/ProfileSmallCardV2.tsx
@@ -20,23 +20,26 @@ export const ProfileSmallCardV2 = ({ profile, topRightSlot }: Props) => {
   return (
     <article className={styles.Container}>
       {topRightSlot && <div className={styles.TopRightSlot}>{topRightSlot}</div>}
+      {profile.status === 'IN_RELATIONSHIP' && <span className={styles.StatusChip}>연애 중</span>}
       <img className={styles.Image} src={profile.images[0]?.url} alt={`${profile.name} 대표 사진`} />
-      <p className={styles.Name}>{profile.name}</p>
-      <div className={styles.InfoWrapper}>
-        <div className={styles.InfoRow}>
-          <Person className={styles.Icon} color={'#ccc'} fontSize={16} />
-          <span>{calculateAge(new Date(profile.birthDate))}세</span>
-          <span>{t(profile.gender)}자</span>
-          <span>{profile.height}cm</span>
-        </div>
-        <div className={styles.InfoRow}>
-          <Location className={styles.Icon} color={'#ccc'} fontSize={16} />
-          {location && (
-            <span>
-              {getLocationText(location, t)}
-              {totalLocationCount > 1 && ` 외 ${totalLocationCount - 1}`}
-            </span>
-          )}
+      <div className={styles.ContentWrapper}>
+        <p className={styles.Name}>{profile.name}</p>
+        <div className={styles.InfoWrapper}>
+          <div className={styles.InfoRow}>
+            <Person className={styles.Icon} color={'#ccc'} fontSize={16} />
+            <span>{calculateAge(new Date(profile.birthDate))}세</span>
+            <span>{t(profile.gender)}자</span>
+            <span>{profile.height}cm</span>
+          </div>
+          <div className={styles.InfoRow}>
+            <Location className={styles.Icon} color={'#ccc'} fontSize={16} />
+            {location && (
+              <span>
+                {getLocationText(location, t)}
+                {totalLocationCount > 1 && ` 외 ${totalLocationCount - 1}`}
+              </span>
+            )}
+          </div>
         </div>
       </div>
     </article>
