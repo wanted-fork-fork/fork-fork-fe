3a57e1b8a0d5f639c9b4b6d9b13e968e0e104d00
조예진 <orobos654@gmail.com>
2026-02-21 17:22:04 +0900

    feat: use searchInfo with no filter

--- src/app/routes/infos.tsx
+++ src/app/routes/infos.tsx
@@ -1,7 +1,7 @@
 import { json, LoaderFunction } from '@remix-run/node';
 import { filterAlignList, filterSchema } from 'src/entities/candidates/_common/libs/filter';
 import { calculateBirthDate, convertDateObjectToDate } from 'src/shared/functions/date';
-import { getAllInfo, searchInfo, SearchInfoParams, SearchInfoRequestDto } from 'src/types';
+import { searchInfo, SearchInfoParams, SearchInfoRequestDto } from 'src/types';
 import { authenticate } from 'src/app/server/authenticate';
 import { commitSession } from 'src/app/server/sessions';
 
@@ -15,12 +15,18 @@ export const loader: LoaderFunction = async ({ request }) => {
   const townList = searchParams.get('townList')?.split(',').filter(Boolean) ?? [];
   const hasFilter = townList.length > 0 || (filterParams && Object.keys(filterParams).length > 0);
 
+  let reqParams: Omit<SearchInfoRequestDto, 'townList'> = {
+    page: 0,
+    size: 10,
+  };
+
   if (hasFilter) {
     const align = filterAlignList.find((ali) => ali.id === filterParams?.alignId) ?? filterAlignList[0];
     const { ageFrom: ageFromValue, ageTo: ageToValue } = filterParams ?? {};
     const ageFrom = ageFromValue ? convertDateObjectToDate(calculateBirthDate(ageFromValue)).toISOString() : undefined;
     const ageTo = ageToValue ? convertDateObjectToDate(calculateBirthDate(ageToValue)).toISOString() : undefined;
-    const params = {
+    reqParams = {
+      ...reqParams,
       ...filterParams,
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
@@ -31,37 +37,25 @@ export const loader: LoaderFunction = async ({ request }) => {
       sortDirection: align.sortDirection,
       ageTo: ageFrom,
       ageFrom: ageTo,
-    } satisfies Omit<SearchInfoRequestDto, 'townList'>;
-    const { data } = await searchInfo(params as unknown as SearchInfoParams, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    return json(
-      {
-        profileList: data?.infos ?? [],
-        totalCount: data?.count ?? 0,
-        hasMore: data?.infos.length >= 10,
-      },
-      {
-        headers: {
-          ...(newSession && { 'Set-Cookie': await commitSession(newSession) }),
-        },
-      },
-    );
-  } else {
-    const { data } = await getAllInfo({
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    return json(
-      { profileList: data, hasMore: false, totalCount: data.length },
-      {
-        headers: {
-          ...(newSession && { 'Set-Cookie': await commitSession(newSession) }),
-        },
-      },
-    );
+    };
   }
+
+  const { data } = await searchInfo(reqParams as unknown as SearchInfoParams, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return json(
+    {
+      profileList: data?.infos ?? [],
+      totalCount: data?.count ?? 0,
+      hasMore: data?.infos.length >= 10,
+    },
+    {
+      headers: {
+        ...(newSession && { 'Set-Cookie': await commitSession(newSession) }),
+      },
+    },
+  );
 };
