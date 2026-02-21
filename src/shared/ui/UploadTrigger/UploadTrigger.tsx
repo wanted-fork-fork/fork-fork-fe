8fab8f1abc7cd3ecd8f2615063e5828405b5aafe
조예진 <orobos654@gmail.com>
2026-02-21 17:28:03 +0900

    fix: filter images

--- src/shared/ui/UploadTrigger/UploadTrigger.tsx
+++ src/shared/ui/UploadTrigger/UploadTrigger.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, ReactElement, useRef } from 'react';
 import styles from './UploadTrigger.module.css';
+import toast from 'react-hot-toast';
 
 type UploadTriggerProps = {
   children: (onTriggerUpload: () => void) => ReactElement;
@@ -16,7 +17,15 @@ export const UploadTrigger = ({ onUploadFiles, accept, multiple, max, children }
 
   const onFileChanged = (e: ChangeEvent<HTMLInputElement>) => {
     const fileUploaded = Array.from(e.target.files ?? []);
-    onUploadFiles(fileUploaded);
+    const filtered = fileUploaded
+      .filter((file) => file.type.startsWith('image/'))
+      .filter((file) => file.size < 3 * 1024 * 1024);
+
+    if (filtered.length !== fileUploaded.length) {
+      toast('5MB 미만의 사진 파일을 업로드해주세요');
+    }
+
+    onUploadFiles(filtered);
   };
 
   return (
